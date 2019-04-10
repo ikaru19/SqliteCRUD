fc00fbf434627970ec3025d9611e46665fed66ba
ikaru19 <syafrizalizal17@gmail.com>
2019-04-10 18:15:39 +0700

    tugas

---
+++ app/src/main/java/id/ac/polinema/notesapp/dao/NoteDao.java
@@ -0,0 +1,28 @@
+package id.ac.polinema.notesapp.dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+import id.ac.polinema.notesapp.models.Note;
+import id.ac.polinema.notesapp.models.User;
+
+@Dao
+public interface NoteDao {
+    @Insert
+    void insert(Note... notes);
+
+    @Update
+    void update(Note... notes);
+
+    @Delete
+    void delete(Note... notes);
+
+    @Query("SELECT * FROM notes")
+    LiveData<List<Note>> getAll();
+}
